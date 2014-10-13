33fd4385d5d6ac0b97cf355ea2c122770687ffd0
Robbert Korving <r.korving@gmail.com>
2014-10-13 20:20:38 +0200

    proxies done!

---
+++ 06_proxy.js
@@ -0,0 +1,21 @@
+var Hapi = require('hapi');
+var path = require('path');
+
+// Run server at default localhost:8080 or accept a port from commandline, parse
+// it as a Number object.
+var server = new Hapi.Server('localhost', Number(process.argv[2] || 8080));
+
+server.route({
+    method: '*',
+    path: '/proxy',
+    handler: {
+        proxy: {
+            host: '127.0.0.1',
+            port: 65535
+        }
+    }
+});
+
+server.start(function () {
+    console.log('Server running at:', server.info.uri);
+});
