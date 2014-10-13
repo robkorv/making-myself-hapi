734a69f4a1a31cfb41e0c0c1bc6d658cbafc7413
Robbert Korving <r.korving@gmail.com>
2014-10-13 20:13:10 +0200

    views done!

---
+++ 05_views.js
@@ -0,0 +1,30 @@
+var Hapi = require('hapi');
+var path = require('path');
+
+// Setup the server with handlebars template support
+var options = {
+    views: {
+        path: path.join(__dirname, '/templates'),
+        engines: {
+            html: require('handlebars')
+        }
+    }
+};
+
+// Run server at default localhost:8080 or accept a port from commandline, parse
+// it as a Number object.
+var server = new Hapi.Server('localhost',
+                             Number(process.argv[2] || 8080),
+                             options);
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: {
+        view: 'layout.html'
+    }
+});
+
+server.start(function () {
+    console.log('Server running at:', server.info.uri);
+});
